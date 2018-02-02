cbf64e003695ea10faa2007113792cd577ae6dff
zhalice2011 <zhalice2012@gmail.com>
2018-02-03 00:11:39 +0800

    parcel压缩代码 部署实验

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
 app.use('/users', users);
 
